972e525cdb9bad199615746c67729e4a97f1bb64
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-11-14 17:09:25 +0100

    👌 IMPROVE: delete default css file, add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react'
 import Header from './components/header'
 
-import logo from './logo.svg'
-import './App.css'
-
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <main className="[ flow ]">
+      <h1 className="App-header">
         <Header text="This is the header" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from TypeScript</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </h1>
+      <p>Hello from TypeScript</p>
+    </main>
   )
 }
 
