ee04dd12d74729a836e96cbe091aafbfb9b050d9
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-11-15 11:49:46 +0100

    📦 NEW: create skeleton for app, add type definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import Header from './components/header'
+import React from 'react';
+import Header from './components/header';
+import SearchForm from './components/searchForm';
+
+type Cocktail = {
+  cocktailId: string;
+  cocktailName: string;
+};
+
+const dummyCocktails: Cocktail[] = [
+  { cocktailId: '11007', cocktailName: 'Margarita' },
+  { cocktailId: '5652', cocktailName: 'Rum Coke' },
+  { cocktailId: '7876', cocktailName: 'Caipirinha' },
+  { cocktailId: '1572', cocktailName: 'Long Island Ice Tea' },
+];
 
 const App: React.FC = () => {
   return (
-    <main className="[ flow ]">
-      <h1 className="App-header">
-        <Header text="This is the header" />
-      </h1>
-      <p>Hello from TypeScript</p>
+    <main className="center text-center">
+      <div className="[ flow ]">
+        <h1 className="App-header">
+          <Header text="Search Cocktails" />
+        </h1>
+        <SearchForm />
+        <section className="list">
+          <ul className="[flow]">
+            {dummyCocktails.map((cocktail, index) => (
+              <li
+                className={`box ${index % 2 !== 0 && 'box__invert'}`}
+                key={cocktail.cocktailId}
+              >
+                {cocktail.cocktailName}
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/searchForm.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SearchForm: React.FC = () => <button className="button">Click Me</button>;
+
+export default SearchForm;
