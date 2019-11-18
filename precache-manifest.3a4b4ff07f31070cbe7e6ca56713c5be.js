61cb02dc0af10345117258efe689d212274c15c3
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-11-18 14:50:59 +0100

    Updates

---
+++ precache-manifest.3a4b4ff07f31070cbe7e6ca56713c5be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fdc2f86faadaa13ffba26da98b40375",
+    "url": "/typescript-react-cocktails/index.html"
+  },
+  {
+    "revision": "9291a8b6968cdc9daa5c",
+    "url": "/typescript-react-cocktails/static/css/main.625ff7ab.chunk.css"
+  },
+  {
+    "revision": "337f4af1826d124610e6",
+    "url": "/typescript-react-cocktails/static/js/2.c9634ee4.chunk.js"
+  },
+  {
+    "revision": "9291a8b6968cdc9daa5c",
+    "url": "/typescript-react-cocktails/static/js/main.b7a8d772.chunk.js"
+  },
+  {
+    "revision": "4661003b7877666e487c",
+    "url": "/typescript-react-cocktails/static/js/runtime-main.0294f5d4.js"
+  }
+]);
